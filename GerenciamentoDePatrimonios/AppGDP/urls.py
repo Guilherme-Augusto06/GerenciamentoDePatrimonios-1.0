e8b14d8bc0897fedb7902d2337b227defaaa6d15
Guilherme-Augusto06 <ga375464@gmail.com>
2024-08-29 16:03:55 -0300

    feat: criação da rota de cadastro

--- GerenciamentoDePatrimonios/AppGDP/urls.py
+++ GerenciamentoDePatrimonios/AppGDP/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
         path('', views.homepage, name="homepage"),         # Inclui as urls do app blog
         path('homepageDark', views.homepageDark, name="homepageDark"),   # Inclui as urls do app blog
         path('login', views.login, name="login"),           # Inclui as urls do app blog
+        path('cadastro', views.cadastro, name="cadastro"),   # Inclui as urls do app blog
 ]
