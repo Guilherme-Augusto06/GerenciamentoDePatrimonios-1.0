5a232d285ac99f3080bd50fe2dd3f4154b3d2b03
Guilherme-Augusto06 <ga375464@gmail.com>
2024-08-29 08:00:28 -0300

    feat: criação da view para a homepageDark

--- GerenciamentoDePatrimonios/AppGDP/views.py
+++ GerenciamentoDePatrimonios/AppGDP/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'homepage.html')
+
+def homepageDark(request):
+    return render(request, 'homepageDark.html')
