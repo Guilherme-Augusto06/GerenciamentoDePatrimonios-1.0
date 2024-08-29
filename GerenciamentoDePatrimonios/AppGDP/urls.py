7908fb2a0e4a9484658189507197a6c945450b8f
Guilherme-Augusto06 <ga375464@gmail.com>
2024-08-29 08:10:51 -0300

    fix: corrigindo um bug no nome da url da homepageDark

--- GerenciamentoDePatrimonios/AppGDP/urls.py
+++ GerenciamentoDePatrimonios/AppGDP/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
         path('', views.homepage, name="homepage"),         # Inclui as urls do app blog
-        path('darkMode', views.homepageDark, name="homepageDark"),   # Inclui as urls do app blog
+        path('homepageDark', views.homepageDark, name="homepageDark"),   # Inclui as urls do app blog
 ]
