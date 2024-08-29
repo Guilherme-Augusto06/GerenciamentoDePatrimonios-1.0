916988549a9f9b7f97cbee0094484de0b24439b9
Guilherme-Augusto06 <ga375464@gmail.com>
2024-08-29 15:39:29 -0300

    feat: criação da rota para tela de login

--- GerenciamentoDePatrimonios/AppGDP/urls.py
+++ GerenciamentoDePatrimonios/AppGDP/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
         path('', views.homepage, name="homepage"),         # Inclui as urls do app blog
         path('homepageDark', views.homepageDark, name="homepageDark"),   # Inclui as urls do app blog
+        path('login', views.login, name="login"),           # Inclui as urls do app blog
 ]
