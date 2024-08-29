c6c455fc08e83dce176e9d21f71b6ace2f6036bd
Guilherme-Augusto06 <ga375464@gmail.com>
2024-08-29 10:52:58 -0300

    feat: Alterações no estilo do css, para os botões da navbar

--- GerenciamentoDePatrimonios/static/js/main.js
+++
@@ -1,7 +0,0 @@
-const darkMode = document.querySelector('.darkModeHomepage');
-function darkModeHomepage() {
-    document.body.classList.toggle('dark-mode');
-}
-darkMode.addEventListener('click', darkModeHomepage);
-
-
